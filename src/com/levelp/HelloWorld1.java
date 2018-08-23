7dfff8a6f0f0c70e2b7a8cbfccaa607a229fae48
Sergey Kostin <s_kostin@yahoo.com>
2018-08-23 23:08:01 +0300

    Два java-файла

---
+++ src/com/levelp/HelloWorld1.java
@@ -0,0 +1,7 @@
+package com.levelp;
+
+public class HelloWorld1 (
+        public static void main (String[] args)
+        System.out.println ("Hello, world1")
+        )
+}
---
+++ src/com/levelp/HelloWorld2.java
@@ -0,0 +1,7 @@
+package com.levelp;
+
+public class HelloWorld2 {
+    public static void (String[] args)(
+            System.out. println("Hello, world2")
+            )
+}
